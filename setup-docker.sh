ad51c5c3de2d2b61aa4a44e3aad8c52f2b5296bc
Domuh <47814388+Hadi-a@users.noreply.github.com>
2024-06-24 02:06:31 +0500

    Create setup-docker.sh
    bash script to install docker and docker compose into your Ubuntu server

---
+++ setup-docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo install -y git curl
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release --codename --short) stable"
+sudo apt update
+sudo apt install docker-ce -y
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.27.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose 
